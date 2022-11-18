2e3e21983c0704cf609b94d12aeed29e6e4c5cc6
saurav <kumarsaurav26198@gmail.com>
2022-11-18 17:44:13 +0530

    imgly woeking for Android

--- App.js
+++ App.js
@@ -15,7 +15,10 @@ import {
   Text,
   useColorScheme,
   View,
+  TouchableHighlight
 } from 'react-native';
+
+import { VESDK, Configuration } from 'react-native-videoeditorsdk';
 
 import {
   Colors,
@@ -56,6 +59,112 @@ const Section = ({ children, title }) => {
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const openEditor = () => {
+    // Set up sample video
+    let video = require('./assets/DJ.mp4');
+    // Set up configuration
+    let configuration: Configuration = {
+      // Configure sticker tool
+      sticker: {
+        // Enable personal stickers
+        personalStickers: true,
+        // Configure sticker library
+        categories: [
+          // Create sticker category with stickers
+          {
+            identifier: 'example_sticker_category_logos',
+            name: 'Logos',
+            thumbnailURI: require('./assets/React.png'),
+            items: [
+              {
+                identifier: 'example_sticker_logos_react',
+                name: 'React',
+                stickerURI: require('./assets/React.png'),
+              },
+              {
+                identifier: 'example_sticker_logos_imgly',
+                name: 'IMG.LY',
+                stickerURI: require('./assets/Igor.png'),
+              },
+            ],
+          },
+          // Reorder and use existing sticker categories
+          { identifier: 'imgly_sticker_category_animated' },
+          { identifier: 'imgly_sticker_category_emoticons' },
+          // Modify existing sticker category
+          {
+            identifier: 'imgly_sticker_category_shapes',
+            items: [
+              { identifier: 'imgly_sticker_shapes_badge_01' },
+              { identifier: 'imgly_sticker_shapes_arrow_02' },
+              { identifier: 'imgly_sticker_shapes_spray_03' },
+            ],
+          },
+        ],
+      },
+      // Configure video composition tool
+      composition: {
+        // Enable personal video clips
+        personalVideoClips: true,
+        // Configure video clip library
+        categories: [
+          // Create video clip category with video clips
+          {
+            identifier: "example_video_category_custom",
+            name: "Custom",
+            items: [
+              {
+                identifier: "example_video_custom_dj",
+                videoURI: require('./assets/DJ.mp4')
+              },
+              {
+                identifier: "example_video_custom_notes",
+                videoURI: require('./assets/Notes.mp4')
+              },
+              {
+                identifier: "example_video_custom_skater",
+                videoURI: require('./assets/Skater.mp4')
+              },
+            ]
+          }
+        ]
+      },
+      // Configure audio tool
+      audio: {
+        // Configure audio clip library
+        categories: [
+          // Create audio clip category with audio clips
+          {
+            identifier: "example_audio_category_custom",
+            name: "Custom",
+            items: [
+              {
+                // Use metadata to display title and artist
+                identifier: "example_audio_custom_elsewhere",
+                audioURI: require('./assets/Elsewhere.mp3')
+              },
+              {
+                // Override metadata to display title and artist
+                identifier: "example_audio_custom_danceharder",
+                title: "Dance Harder",
+                artist: "Three Chain Links",
+                audioURI: require('./assets/DanceHarder.mp3')
+              }
+            ]
+          }
+        ]
+      }
+    };
+    VESDK.openEditor(video, configuration).then(
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
+  };
+
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
@@ -74,6 +183,19 @@ const App = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <Section title="VideoEditor SDK">
+            <TouchableHighlight onPress={openEditor}>
+              <Text
+                style={[
+                  styles.sectionDescription,
+                  {
+                    color: isDarkMode ? Colors.white : Colors.black,
+                  },
+                ]}>
+                Click here to <Text style={styles.highlight}>edit a sample video</Text>.
+              </Text>
+            </TouchableHighlight>
+          </Section>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
